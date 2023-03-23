b03b39809bb462befe30f79efbb947ae3c48410f
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-03-23 16:23:04 -0300

    Update

--- src/main/java/edu/bootcamp/services/IBootcamp.java
+++ src/main/java/edu/bootcamp/services/IBootcamp.java
@@ -2,6 +2,7 @@ package edu.bootcamp.services;
 
 import edu.bootcamp.model.Bootcamp;
 import edu.bootcamp.model.Course;
+import edu.bootcamp.model.Teacher;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface IBootcamp {
     public void createBootcamp(Bootcamp bootcamp);
     public List<String> getAllStudent();
     public List<Course> getAllCourse();
+    public List<Teacher> getAllTeacher();
     public Integer getNumberOfStudent();
 }
