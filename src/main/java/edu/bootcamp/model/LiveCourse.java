53b7a38efd711a35e23ad231a2fcbd316e918ddf
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-03-23 15:46:55 -0300

    Implemented the LiveCourse domain model

--- src/main/java/edu/bootcamp/model/LiveCourse.java
+++ src/main/java/edu/bootcamp/model/LiveCourse.java
@@ -1,4 +1,39 @@
 package edu.bootcamp.model;
 
-public class LiveCourse {
+import edu.bootcamp.utility.IDUtil;
+
+import java.time.LocalDate;
+
+public class LiveCourse extends Course{
+    private LocalDate date;
+    private String platform;
+    private String link;
+
+    public LiveCourse() {
+        this.setId(IDUtil.uniqueID(this.getTitle()));
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
