a905fe4144007f75d87fd016b8515580de10a926
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-03-23 16:12:55 -0300

    Implemented an interface for bootcamp

--- src/main/java/edu/bootcamp/services/IBootcamp.java
+++ src/main/java/edu/bootcamp/services/IBootcamp.java
@@ -1,4 +1,13 @@
 package edu.bootcamp.services;
 
+import edu.bootcamp.model.Bootcamp;
+import edu.bootcamp.model.Course;
+
+import java.util.List;
+
 public interface IBootcamp {
+    public void createBootcamp(Bootcamp bootcamp);
+    public List<String> getAllStudent();
+    public List<Course> getAllCourse();
+    public Integer getNumberOfStudent();
 }
