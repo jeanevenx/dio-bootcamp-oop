c36b9009564de0e2bc8ea32f3ed9b7bdabb69acc
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-03-23 18:45:07 -0300

    Implemented some services for the class "Bootcamp"

---
+++ src/main/java/edu/bootcamp/services/BootcampServiceImpl.java
@@ -0,0 +1,63 @@
+package edu.bootcamp.services;
+
+import edu.bootcamp.model.*;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BootcampServiceImpl implements IBootcamp{
+    int numberOfStudent = 0;
+    @Override
+    public void createBootcamp(Bootcamp bootcamp) {
+
+        Set<Bootcamp> bootcampSet = new LinkedHashSet<>();
+
+            bootcampSet.add(bootcamp);
+    }
+
+    @Override
+    public double GetTotalHour() {
+        Bootcamp course = new Bootcamp();
+
+        double totalOfHour = 0.0;
+
+        for(int i = 0; i <= course.getCourseSet().size(); i++){
+            totalOfHour += course.getCourseSet().iterator().next().getNumberOfHour();
+        }
+        return totalOfHour;
+    }
+
+    @Override
+    public List<String> getAllStudent() {
+        List<String> studentList = new ArrayList<>();
+        Bootcamp student = new Bootcamp();
+
+        for (int i = 0; i <= student.getStudentSet().size(); i++) {
+            studentList.add(student.getStudentSet().iterator().next().getName());
+        }
+
+        return studentList;
+    }
+
+    @Override
+    public Set<Course> getAllCourse() {
+        Bootcamp course = new Bootcamp();
+
+        return course.getCourseSet();
+    }
+
+    @Override
+    public Set<Teacher> getAllTeacher() {
+        Bootcamp teacher = new Bootcamp();
+        return teacher.getTeacherSet();
+    }
+
+    @Override
+    public Integer getNumberOfStudent() {
+        Bootcamp student = new Bootcamp();
+
+        return student.getStudentSet().size();
+    }
+}
