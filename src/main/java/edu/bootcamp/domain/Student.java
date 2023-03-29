19d4eb08aa51e70f98b7b8990caaa3eb8fdbde38
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-03-29 19:04:37 -0300

    Added 2 methods to student class

--- src/main/java/edu/bootcamp/domain/Student.java
+++ src/main/java/edu/bootcamp/domain/Student.java
@@ -1,22 +1,55 @@
 package edu.bootcamp.domain;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 public class Student {
     private String id;
     private String name;
     private String username;
     private String email;
-    private List<Bootcamp> bootcampList = new ArrayList<>();
+    private Set<String> bootcampEnrolled = new LinkedHashSet<>();
+    private Set<String> bootcampCompleted = new LinkedHashSet<>();
+
+
+    public void enrollBootcamp(Bootcamp bootcamp){
+        this.bootcampEnrolled.add(bootcamp.getName());
+        bootcamp.getStudentSet().add(this.name);
+    }
+
+    public void makeCourse(Course course){
+        boolean status =course.getStatus().equals("unseen");
+
+        if(status){
+            course.setStatus("seen");
+        }
+    }
+    public  void progress(Bootcamp bootcamp){
+        boolean isCompleted = false;
+        String status = bootcamp.getCourseSet().iterator().next().getStatus();
+
+        while(bootcamp.getCourseSet().iterator().hasNext()){
+            if(status.equals("seen")){
+                isCompleted = true;
+            } else {
+                isCompleted = false;
+            }
+        }
+
+        if (isCompleted){
+            bootcampCompleted.add(bootcamp.getName());
+        }
+    }
 
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public final void setId(String id) {
         this.id = id;
     }
 
@@ -44,13 +77,34 @@ public class Student {
         this.email = email;
     }
 
-    public List<Bootcamp> getBootcampList() {
-        return bootcampList;
+    public Set<String> getBootcampEnrolled() {
+        return bootcampEnrolled;
     }
 
-    public void setBootcampList(List<Bootcamp> bootcampList) {
-        this.bootcampList = bootcampList;
+    public void setBootcampEnrolled(Set<String> bootcampEnrolled) {
+        this.bootcampEnrolled = bootcampEnrolled;
     }
+
+    public Set<String> getBootcampCompleted() {
+        return bootcampCompleted;
+    }
+
+    public void setBootcampCompleted(Set<String> bootcampCompleted) {
+        this.bootcampCompleted = bootcampCompleted;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(username, student.username) && Objects.equals(email, student.email) && Objects.equals(bootcampEnrolled, student.bootcampEnrolled) && Objects.equals(bootcampCompleted, student.bootcampCompleted);
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, name, username, email, bootcampEnrolled, bootcampCompleted);
+//    }
 
     @Override
     public String toString() {
@@ -59,7 +113,8 @@ public class Student {
                 ", name='" + name + '\'' +
                 ", username='" + username + '\'' +
                 ", email='" + email + '\'' +
-                ", bootcampSet=" + bootcampList +
+                ", bootcampEnrolled=" + bootcampEnrolled +
+                ", bootcampCompleted=" + bootcampCompleted +
                 '}';
     }
 }
